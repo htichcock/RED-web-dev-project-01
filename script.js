3d51362d84ac6868ecead241ae9e4c4252c9c9a8
Ian <ianhitchcock@outlook.com>
2017-11-28 14:11:34 -0800

    Format code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   $('.main-carousel').flickity({
     // options
     wrapAround: true,
@@ -9,25 +9,25 @@ $(function(){
   });
 
   //email handler
-  $('#email-subscribe').on('click', function(e){
+  $('#email-subscribe').on('click', function (e) {
     e.preventDefault();
-     //reg exp from http://regexlib.com/REDetails.aspx?regexp_id=26
+    //reg exp from http://regexlib.com/REDetails.aspx?regexp_id=26
     var regExpEmail = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     if (regExpEmail.test($('#email-input').val())) {
-      $('#email-input').val("").attr("placeholder","Thank you for subcribing!");
+      $('#email-input').val("").attr("placeholder", "Thank you for subcribing!");
       alert('Thank you for subscribing!');
     } else {
-      $('#email-input').val("").attr("placeholder","Invalid Address");
+      $('#email-input').val("").attr("placeholder", "Invalid Address");
       alert('Please enter valid email address');
     }
-    $('#email-input').on('focus', function(event){
+    $('#email-input').on('focus', function (event) {
       event.preventDefault();
       $('this').val('').attr('placeholder', 'Your Email');
     });
   });
 
   //smooth scrolling with the help of https://css-tricks.com/snippets/jquery/smooth-scrolling/
-  $('a[href*="#"]').not('a[href="#"]').on('click', function(event){
+  $('a[href*="#"]').not('a[href="#"]').on('click', function (event) {
     // Figure out element to scroll to
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -37,7 +37,7 @@ $(function(){
       event.preventDefault();
       $('html, body').animate({
         scrollTop: target.offset().top - $('header').outerHeight()
-      }, 1000, function() {
+      }, 1000, function () {
         // Callback after animation
         // Must change focus!
         var $target = $(target);
@@ -45,10 +45,10 @@ $(function(){
         if ($target.is(":focus")) { // Checking if the target was focused
           return false;
         } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
           $target.focus(); // Set focus again
         }
       });
     }
-  });  
+  });
 });
