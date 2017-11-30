5aa5388412f805ad634f33e63028f9b4ff0cbacb
Ian <ianhitchcock@outlook.com>
2017-11-30 11:20:18 -0800

    Email alert fix

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(function () {
       $('#email-input').val("").attr("placeholder", "Thank you for subcribing!");
       alert('Thank you for subscribing!');
     } else {
-      $('#email-input').val("").attr("placeholder", "Invalid Address");
+      $('#email-input').attr("placeholder", "Invalid Address");
       alert('Please enter valid email address');
     }
     $('#email-input').on('focus', function (event) {
@@ -42,12 +42,6 @@ $(function () {
         // Must change focus!
         var $target = $(target);
         $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        }
       });
     }
   });
